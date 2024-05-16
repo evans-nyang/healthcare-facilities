c58632bd8cb5aff5a8a6f328b9972e803ad17359
evans-nyang <nyanggithub@gmail.com>
2024-05-16 23:59:55 +0300

    Initial commit

---
+++ mage/mage-healthcare/data_loaders/data_extraction.py
@@ -0,0 +1,29 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    url = "https://data.humdata.org/dataset/7b9c2851-dc37-4a88-9dcb-62e55eb91baf/resource/df6bfc55-3b25-4309-a1b4-74afba434956/download/kenya-health-facilities-2017_08_02.xlsx"
+
+# mydata = requests.get(url)
+    response = requests.get(url)
+
+    # return pd.read_csv(io.StringIO(response.text), sep=',')
+    return pd.read_excel(response)
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
