5bd8ac0426b9811178ae1618ddef50ab456014ce
evans-nyang <nyanggithub@gmail.com>
2024-05-23 23:58:13 +0300

    Add data ingestion pipelines in mage

--- mage/mage-healthcare/data_loaders/data_extraction.py
+++ mage/mage-healthcare/data_loaders/data_extraction.py
@@ -14,11 +14,9 @@ def load_data_from_api(*args, **kwargs):
     """
     url = "https://data.humdata.org/dataset/7b9c2851-dc37-4a88-9dcb-62e55eb91baf/resource/df6bfc55-3b25-4309-a1b4-74afba434956/download/kenya-health-facilities-2017_08_02.xlsx"
 
-# mydata = requests.get(url)
     response = requests.get(url)
 
-    # return pd.read_csv(io.StringIO(response.text), sep=',')
-    return pd.read_excel(response)
+    return pd.read_excel(io.BytesIO(response.content))
 
 
 @test
