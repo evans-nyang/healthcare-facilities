8211e86c7772494744cc5e28c39f5bdcece50ce9
Johnevans <85787114+evans-nyang@users.noreply.github.com>
2024-06-07 20:52:28 +0000

    Change column names, use underscore instead of camelCase.

--- mage/healthcare/dbt/health_base/models/staging/dimensions/stg_facilities.sql
+++ mage/healthcare/dbt/health_base/models/staging/dimensions/stg_facilities.sql
@@ -4,11 +4,11 @@ WITH stg_facilities AS (
 
 staged AS (
     SELECT
-        md5(trim(_name)) AS name_hash,
-        lower(_name) AS facilityName,
-        lower(facility_type) AS facilityType,
+        md5(trim(_name)) AS facility_hash,
+        lower(_name) AS facility_name,
+        lower(facility_type) AS facility_type,
         lower(_owner) AS ownership,
-        lower(regulatory_body) AS regulatoryBody
+        lower(regulatory_body) AS regulatory_body
     FROM stg_facilities
 )
 
